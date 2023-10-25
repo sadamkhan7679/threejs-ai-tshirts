459c48c6434fad24659184d2727d641807866f6d
Sadam Hussain <d_shussain@universalweather.net>
2023-10-24 23:07:11 -0400

    first commit

---
+++ src/canvas/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface CanvasProps {}
+
+const Canvas: React.FC<CanvasProps> = () => {
+  return <div>Canvas</div>;
+};
+
+export default Canvas;
---
+++ src/pages/Customizer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface CustomizerProps {}
+
+const Customizer: React.FC<CustomizerProps> = ({}) => {
+  return (
+    <div>
+      {" "}
+      <p>Welcome to the customizer page!</p>
+    </div>
+  );
+};
+
+export default Customizer;
